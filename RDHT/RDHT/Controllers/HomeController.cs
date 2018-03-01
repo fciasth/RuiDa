2cc64119fc3ff9b00d9fd44ff33607f39251a99a
zxh <zxh_sea@outlook.com>
2018-03-01 14:07:41 +0800

    1

--- RDHT/RDHT/Controllers/HomeController.cs
+++ RDHT/RDHT/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using RuiDa1.Tools;
+﻿using RDHT.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace RuiDa1.Controllers
+namespace RDHT.Controllers
 {
    
     public class HomeController : Controller
